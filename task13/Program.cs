0f46ce00f8b13e46049b1ee7d8858216ed6f2ab6
[sholpan] <[sholpanismailova@gmail.com]>
2022-09-17 01:10:05 +0600

    added homework2

---
+++ task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = num;
+if (num < 100)
+{Console.WriteLine($"{num} -> третьей цифры нет");}
+else 
+{while (count > 1000)
+{ 
+    count = count / 10; 
+}
+Console.WriteLine($"{num} -> {count % 10}");}
+
---
+++ task15/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write ("Введите цифру от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 7 || num == 6) Console.WriteLine($"{num} -> Да");
+else if (num >=1 && num <=5) Console.WriteLine($"{num} -> Нет");
+else Console.WriteLine("Такого дня недели не существует");
