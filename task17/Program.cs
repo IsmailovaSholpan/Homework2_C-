d7ecec8cec9f8c5daafa80a9362a2745272b6dcf
[sholpan] <[sholpanismailova@gmail.com]>
2022-09-25 01:14:21 +0600

    added tasks

---
+++ task17/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напиши программу, которая на вход принимает координаты точки (Х; Y), 
+// причем Х не равно 0 и Y не равно 0, и выдает номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите координаты точки Х");
+Console.Write("Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int xc, int yc)
+{ if (xc > 0 && yc > 0) return "Первая четверь";
+    if (xc > 0 && yc < 0) return "Вторая четверь";
+    if (xc < 0 && yc < 0) return "Третья четверь";
+    if (xc < 0 && yc > 0) return "Четвертая четверь";
+    return "Введены некорректные координаты";
+}
+string result = Quarter(x, y);
+Console.WriteLine(result);
---
+++ task20/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает координаты двух точек
+// и находит расстояние между ними в 2Д пространстве.
+
+Console.WriteLine("Введите координаты точки А");
+Console.Write("Х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+Console.Write("Х2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+double Distance(int xc1, int yc1, int xc2, int yc2)
+{
+    // if (xc1 > xc2) return a = xc1 - xc2;
+    // if (xc1 < xc2) return a = xc2 - xc1;
+    // if (yc1 > yc2) return b = yc1 - yc2;
+    // if (yc1 < yc2) return b = yc2 - yc1;
+    double c = (xc1 - xc2)*(xc1 - xc2) + (yc1 - yc2)*(yc1 - yc2);
+    return Math.Round(Math.Sqrt(c),2, MidpointRounding.ToZero); 
+}
+
+double result = (Distance(x1, y1, x2, y2));
+Console.WriteLine(result);
+
